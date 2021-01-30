65b85586993c3d47608f0fe5c08af291009e83b9
heeenkie <35926672+heeenkie@users.noreply.github.com>
2021-01-30 13:18:50 +0100

    create lic1

--- src/CMV.java
+++ src/CMV.java
@@ -3,19 +3,22 @@
 
  */
 import javafx.util.Pair;
+
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 
 public class CMV {
     private boolean[] vector;
     private List<Pair<Double,Double>> points;
-    private int numpoints;
+    private int numPoints;
     private Parameters parameters;
     //TODO add parameters to constructer
-    public CMV (List<Pair<Double,Double>> points , int numpoints, Parameters parameters){
+    public CMV (List<Pair<Double,Double>> points , int numPoints, Parameters parameters){
 
         this.points = points;
-        this.numpoints = numpoints;
+        this.numPoints = numPoints;
         this.parameters = parameters;
 
         vector = createCMV();
@@ -28,5 +31,29 @@ public class CMV {
             // call all LICS
         return null;
     }
-    
+
+
+    public boolean checkLIC1(){
+        if (numPoints < 3) return false;
+        double radius1 = parameters.RADIUS1;
+
+        for (int i = 0; i < numPoints-2; i++) {
+           double distance1 = Math.sqrt(Math.pow(points.get(i).getKey() - points.get(i+1).getKey(), 2) + Math.pow(points.get(i).getValue() - points.get(i+1).getValue(), 2));
+           double distance2 = Math.sqrt(Math.pow(points.get(i).getKey() - points.get(i+2).getKey(), 2) + Math.pow(points.get(i).getValue() - points.get(i+2).getValue(), 2));
+           double distance3 = Math.sqrt(Math.pow(points.get(i+1).getKey() - points.get(i+2).getKey(), 2) + Math.pow(points.get(i+1).getValue() - points.get(i+2).getValue(), 2));
+
+           if (distance3 > radius1*2 || distance1 > radius1*2 || distance2 > radius1*2) return true;
+        }
+        
+        Pair<Double, Double> p1 = points.get(0);
+        Pair<Double, Double> p2 = points.get(1);
+        List a = new ArrayList<>();
+        a.add(p2.getKey() - p1.getKey());
+
+
+
+        return false;
+    }
+
+
 }
